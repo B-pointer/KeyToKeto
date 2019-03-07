d227c6447f3f759afd6d7bc8760fec18fafd8618
B-pointer <bkp5@students.uwf.edu>
2019-03-07 11:01:28 -0600

    adding the tab view. Nees to be split up though

--- src/LoginPageController.java
+++ src/LoginPageController.java
@@ -1,7 +1,11 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginPageController {
 	
@@ -12,7 +16,17 @@ public class LoginPageController {
 	@FXML protected void loginClick(ActionEvent event)
 	{
 		//obviously not really what we want to do, just making sure the button is buttoning
-		System.out.println("Login clicked");
-		username.setText("Login Clicked");
+		//System.out.println("Login clicked");
+		//username.setText("Login Clicked");
+		try {
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			Parent newScene = FXMLLoader.load(getClass().getResource("TabFrame.fxml"));
+			stage.getScene().setRoot(newScene);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error changing scenes");
+		}
+		
 	}
 }
