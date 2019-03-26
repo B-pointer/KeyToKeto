1beb25d47aabbcbbe1cbc968539a84dad1592c93
jsc55 <jsc55@students.uwf.edu>
2019-03-26 12:34:04 -0500

    Created read from file

--- src/LoginPageController.java
+++ src/LoginPageController.java
@@ -6,27 +6,49 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class LoginPageController {
 	
 	@FXML private TextField username;
 	@FXML private TextField password;
 	@FXML private Button loginButton;
+	private boolean proceed;
 	
-	@FXML protected void loginClick(ActionEvent event)
+	@FXML protected void loginClick(ActionEvent event) throws FileNotFoundException
 	{
-		//obviously not really what we want to do, just making sure the button is buttoning
-		//System.out.println("Login clicked");
-		//username.setText("Login Clicked");
+		
+		File file = new File("users");
+		Scanner scan = new Scanner(file);
+        while(scan.hasNext())
+        {
+        String usr = scan.next();
+        String pwd = scan.next();
+        if(username.getText().equals(usr) && password.getText().equals(pwd))
+        {
+        	proceed = true;
+        }
+        
 		try {
+			if(proceed == true)
+			{
 			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			Parent newScene = FXMLLoader.load(getClass().getResource("TabFrame.fxml"));
 			stage.getScene().setRoot(newScene);
+			}
+			
 		}
+		
 		catch(Exception e)
 		{
 			System.out.println("Error changing scenes");
 		}
+        }
 		
 	}
 	@FXML protected void createClick(ActionEvent event)
