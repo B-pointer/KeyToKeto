3cc75c05d2d12c60650c2932b5a47fb0292efd41
B-pointer <bkp5@students.uwf.edu>
2019-04-16 14:26:54 -0500

    now auto recalculates the users macros if they edit the calories. Note that the calories are not saved until the user clicks save button

--- src/Controllers/ResultsController.java
+++ src/Controllers/ResultsController.java
@@ -2,13 +2,18 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
 
 import Calculations.KetoCalculations;
 import DataAccess.DataAccessible;
 import Models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextFormatter.Change;
 
 public class ResultsController {
 	@FXML public TextField calories;
@@ -33,12 +38,21 @@ public class ResultsController {
 	
 	@FXML private void initialize()
 	{
-		fillFields(true);
+		setFormatter();
+		//fillFields(true);
+		fillFields(user.getCalories());
+		calories.textProperty().addListener((obs, oldText, newText)-> 
+		{
+			if(newText.length() > 2){
+				fillFields(Integer.parseInt(newText));
+			}
+		});
 	}
  
 	@FXML public void newResults(ActionEvent e)
 	{
-		fillFields(false);
+		int totalCalories = KetoCalculations.calculateCalories(user.getAge(), user.getHeight(), user.getWeight(), user.getGender());
+		fillFields(totalCalories);
 		/*
 		int totalCalories = KetoCalculations.calculateCalories(user.getAge(), user.getHeight(), user.getWeight(), user.getGender());
 		calories.setText(Integer.toString(totalCalories));
@@ -53,35 +67,56 @@ public class ResultsController {
 	}
 	@FXML private void saveClick(ActionEvent e)
 	{
-		System.out.println("need to save to db here and also need to check to be sure field is not empty");
-		user.setCalories(Integer.parseInt(calories.getText()));
+		
+		if(checkFields())
+		{
+			System.out.println("need to save to db here and also need to check to be sure field is not empty");
+			user.setCalories(Integer.parseInt(calories.getText()));
+		}
 	}
 	
 	@FXML private void resetClick(ActionEvent e)
 	{
-		fillFields(true);
+		//fillFields(true);
+		fillFields(user.getCalories());
 	}
 	//if from save is true, then it uses the calories stored in the User object. if it is false, it will recalculate the calories and the other
 	//values based on the User objects current age, height, weight, and gender
-	private void fillFields(boolean fromSave)
+	private void fillFields(int totalCalories)
 	{
-		int totalCalories;
-		if(fromSave)
-		{
-			totalCalories = user.getCalories();
-		}
-		else
-		{
-			totalCalories = KetoCalculations.calculateCalories(user.getAge(), user.getHeight(), user.getWeight(), user.getGender());
-		}
-		
 		calories.setText(Integer.toString(totalCalories));
 		carbs.setText(Integer.toString(KetoCalculations.calculateCarbs(totalCalories)));
 		fats.setText(Integer.toString(KetoCalculations.calculateFat(totalCalories)));
 		proteins.setText(Integer.toString(KetoCalculations.calculateProtein(totalCalories)));
-		// home.setCals(calories.toString());
 		System.out.println(calories.toString());
 		String homeCarbs = String.valueOf((KetoCalculations.calculateCarbs(Integer.parseInt(calories.getText()))));
 		home.setCals(homeCarbs);
+	}
+	
+	private void setFormatter()
+	{
+		UnaryOperator<Change> filter = change ->{
+			String text = change.getText();
+			if(text.matches("[0-9]*"))
+			{
+				return change;
+			}
+			return null;
+		};
+		calories.setTextFormatter(new TextFormatter<String>(filter));
+	}
+	
+	private boolean checkFields()
+	{
+		if(calories.getText().isEmpty())
+		{
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Invalid Entry");
+			alert.setContentText("Calories must not be left empty");
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return false;
+		}
+		return true;
 	}
 }
